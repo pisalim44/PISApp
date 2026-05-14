aea83c45da4ad8f9c7d936e7022a7146525eb784
pisalim44 <thiagodemourajesus@gmail.com>
2026-05-14 12:17:57 -0300

    PISApp API GITHUB

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbwCI9SD_wcJaeXtozZeyNAJ81W0O17X2anhg_iBg3PhOgswcHTqvp6ihYa83ysXFRXJ/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbwq_nljbZoAvCllofhp9FoDziKNslkQkBX-urfQwdjnGiOWqyy7f3b7O_9vC8gslUOM/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
