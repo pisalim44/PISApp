6b024e2e693a89af47efe11473387cc472d0ae49
pisalim44 <thiagodemourajesus@gmail.com>
2026-05-18 14:05:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbx1E1ShG40xsNLTt-qn5c4pkZv51TCBRVCCe3zXGPriTM47pQnpxSb7uzk4B8jvy_kX/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbyrP3x9Worhy3BSw5BGvmyXPJzwF8SGOekZpWP-k-AZ5Shh-zqdnuT40H_yILL5y2dF/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
