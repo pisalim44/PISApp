cbfe9c5dd4777b3a31be553493437d992c58d79f
pisalim44 <thiagodemourajesus@gmail.com>
2026-05-23 08:39:07 -0300

    fix vincular

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbzxpB5uJOAfck5GaN4nJVABb1BhkXSKkzUwLDXzCKD_mLIpIrbTjj_iTJrRip8hb3po/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbyUna55VOj2QT6Ur4uSs2gk_uXFVrqdVGF0B1A3kBMk8fb2bszOgW6Yoab-JUXLocy4/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
