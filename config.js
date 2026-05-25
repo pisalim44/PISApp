b5e8fcdbf14016c913f229016e4201ce2d022f89
pisalim44 <thiagodemourajesus@gmail.com>
2026-05-25 13:26:45 -0300

    FAST

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbyUna55VOj2QT6Ur4uSs2gk_uXFVrqdVGF0B1A3kBMk8fb2bszOgW6Yoab-JUXLocy4/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbydVB1VCzCAr3wvTMgjy8DZnPA_Z_YA11Plk9ITnPy99-6KGa6eF5GilLox_dbAJEPd/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
