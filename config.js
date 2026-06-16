5ce302f79b1119b049775ab0b819037573a2e419
pisalim44 <thiagodemourajesus@gmail.com>
2026-06-16 14:21:11 -0300

    pesquisa.inrad

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbwdPALv_1iVUTOh4XOwgQf-zQYvck6CTr5VZNuEC6waxjBoOnT3e8IcLCt8YAsmjXBr/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbywU90kCjrxjmDGLPoriKw_4NJAFa7aq6MfoC4gtSI413RoFtGLv9_j6tKlWleujLM/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
