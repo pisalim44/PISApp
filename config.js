37bc343ce70f4b32cee0a6a774fb11fa36dcd505
pisalim44 <thiagodemourajesus@gmail.com>
2026-09-24 07:52:56 -0300

    Update PISAPP_API_URL to new endpoint FIX SINC

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbyvdQJMDln8ftyDQW6Z70kyuu3mjFqMTzBbMcgGerVF9NpbyLFnhjWmarFqp6SjQ2vZ/exec';
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbxCmbYT_m1iOH5LCnI0A82qUuZ1ZmJ-dsZWwxYi5XEhgws1FrF-UFSTezpu44anLIot/exec';
 const PISAPP_API_URL = window.PISAPP_API_URL;
